cc55f16921fd1c2653547ac3aee847ca67c77c60
Tom Sparrow <s192010@student.dhbw-mannheim.de>
2022-02-21 22:25:53 +0100

    removed console.log

--- web/backend/controller/test_controller.ts
+++ web/backend/controller/test_controller.ts
@@ -10,17 +10,15 @@ const getUsers = async (ctx: undefined) => {
     const data = await User.find();
     ctx.response.body = { "status": true, data: data };
     ctx.response.status = 200;
-  } catch (err) {
+  } catch (_err) {
     ctx.response.body = { status: false, data: null };
     ctx.response.status = 500;
-    console.log(err);
   }
 };
 
 const createUser = async (ctx: undefined) => {
   try {
     const body = await ctx.request.body();
-    console.log(await body.value);
     const { name, phone, email } = await body.value;
     const id = await User.insertOne({
       name: name,
@@ -29,10 +27,10 @@ const createUser = async (ctx: undefined) => {
     });
     ctx.response.body = { status: true, data: id };
     ctx.response.status = 201;
-  } catch (err) {
+  } catch (_err) {
     ctx.response.body = { status: false, data: null };
     ctx.response.status = 500;
-    console.log(err);
+
   }
 };
 
