d4d6fbc3af05ef275d925cc76aeb000c48ac9156
Tom Sparrow <s192010@student.dhbw-mannheim.de>
2022-02-27 11:43:34 +0100

    fixed typo

--- web/backend/db/mongo.ts
+++ web/backend/db/mongo.ts
@@ -4,7 +4,7 @@ import { config } from "../../../deps/web/deps.ts";
 const env = config();
 const db_name: string = Deno.env.get("DB_NAME") || env.DB_NAME;
 const db_user: string = Deno.env.get("DB_USERNAME") || env.DB_USERNAME;
-const db_password: string = Deno.env.get("BOT_PASSWORD") || env.DB_PASSWORD;
+const db_password: string = Deno.env.get("DB_PASSWORD") || env.DB_PASSWORD;
 const db_uri1: string = Deno.env.get("DB_URI1") || env.DB_URI1;
 const db_uri2: string = Deno.env.get("DB_URI2") || env.DB_URI2;
 const db_uri3: string = Deno.env.get("DB_URI3") || env.DB_URI3;
