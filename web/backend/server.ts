c4b7c5b4201776849c046ba42a1c5c1cdf0c1203
Tom Sparrow <6250143+xTomsko@users.noreply.github.com>
2022-03-02 20:05:39 +0100

    fixed path

--- web/backend/server.ts
+++ web/backend/server.ts
@@ -8,7 +8,7 @@ const app = new Application();
 app.use(async (context, next) => {
   try {
     await context.send({
-      root: `${Deno.cwd()}/../frontend/dist/`,
+      root: `/web/frontend/dist/`,
       index: "index.html",
     });
   } catch {
