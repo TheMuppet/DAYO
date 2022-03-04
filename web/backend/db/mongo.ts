d03a71604d9a22aaa1e4fddf4978d4cb64859233
Fabian Losem <fabian.losem@web.de>
2022-03-04 19:14:59 +0100

    update mongo.ts
    - removed testin schema: UserSchema

--- web/backend/db/mongo.ts
+++ web/backend/db/mongo.ts
@@ -6,22 +6,24 @@ const client = new MongoClient();
 const db_user: string = env.DB_USER;
 const db_password: string = env.DB_PASSWORD;
 const db_name: string = env.DB_NAME;
-const db_uri: string = env.DB_URI;
+const db_uri1: string = env.DB_URI1;
+const db_uri2: string = env.DB_URI2;
+const db_uri3: string = env.DB_URI3;
 
 await client.connect({
   db: db_name,
   tls: true,
   servers: [
     {
-      host: db_uri,
+      host: db_uri1,
       port: 27017,
     },
     {
-      host: db_uri,
+      host: db_uri2,
       port: 27017,
     },
     {
-      host: db_uri,
+      host: db_uri3,
       port: 27017,
     },
   ],
@@ -36,13 +38,4 @@ await client.connect({
 // for testing mongo connection
 const db = client.database(db_name);
 
-// Schema for testing (in future project prob use extra folder for Schemas)
-interface UserSchema {
-  _id: { $oid: string };
-  name: string;
-  email: string;
-  phone: string;
-}
-
-const User = db.collection<UserSchema>("user");
-export { db, User };
+export { db };
