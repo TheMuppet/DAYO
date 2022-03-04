3043cba4f44ddf3909ff8c1b87b58da748168249
alexsc23 <s191825@student.dhbw-mannheim.de>
2022-03-04 14:18:20 +0100

    Prepare frontend for matches

---
+++ web/frontend/src/components/Match.svelte
@@ -0,0 +1,13 @@
+<script>
+    import Card from "@/components/UI/Card.svelte"
+    export let match = {}
+    console.log(match)
+</script>
+  
+<Card>
+    {match.m} & {match.w} are a perfect match!
+</Card>
+
+<style>
+</style>
+  
---
+++ web/frontend/src/components/Matches.svelte
@@ -0,0 +1,18 @@
+<script>
+  import Match from "@/components/Match.svelte"
+  export let matches = [];
+</script>
+
+<div class="matches-list">
+  <h1>Current Matches</h1>
+  <p></p>
+  {#each matches as match (match.id)}
+    <Match {match}/>
+  {/each}
+</div>
+
+<style>
+  .matches-list {
+    padding: 20px;
+  }
+</style>
