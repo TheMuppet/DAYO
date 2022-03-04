a6dca060a216e17455d89be36d0f692375f484e5
Fabian Losem <fabian.losem@web.de>
2022-03-04 19:09:34 +0100

    implements participant controller
    adds functions for:
      - create Participants
      - find Participants (by gender and/or season) or all Participants

---
+++ web/backend/controller/participantController.ts
@@ -0,0 +1,79 @@
+import { Participant } from "../schemas/participant.ts";
+import { Context } from "../../../deps.ts";
+
+// creates a new participant with data in request
+const createParticipant = async (ctx: Context) => {
+  try {
+    const body = await ctx.request.body();
+    const { name, gender, age, picture, season } = await body.value;
+    const data = await Participant.insertOne({
+      name: name,
+      age: age,
+      picture: picture,
+      season: season,
+      gender,
+    });
+    ctx.response.body = { status: true, data: data };
+    ctx.response.status = 201;
+  } catch (_err) {
+    console.log(_err);
+    ctx.response.body = {
+      status: false,
+      error: _err.name,
+      error_message: _err.message,
+    };
+    ctx.response.status = 500;
+  }
+};
+
+// finds and filter participant in mongodb (filter: season, gender, none)
+const findAll = async (ctx: Context) => {
+  let gender;
+  let season;
+  // get data from request body
+  try {
+    const body = await ctx.request.body().value;
+    // handles season input
+    try {
+      season = body.season;
+    } catch {
+      season = undefined;
+    }
+    // handles gender input
+    try {
+      gender = body.gender;
+    } catch {
+      gender = undefined;
+    }
+    // if there is no filter it will just return all participants
+  } catch {
+    season = undefined;
+    gender = undefined;
+  }
+  await findParticipant(ctx, gender, season);
+};
+
+// finds the participants
+const findParticipant = async (
+  ctx: Context,
+  gender: string,
+  season: number,
+) => {
+  try {
+    const allParticipant = await Participant.find({
+      "gender": gender,
+      "season": season,
+    }).toArray();
+    ctx.response.body = { status: true, data: allParticipant };
+    ctx.response.status = 200;
+  } catch (error) {
+    ctx.response.body = {
+      status: false,
+      error: error.name,
+      error_message: error.message,
+    };
+    ctx.response.status = 500;
+  }
+};
+
+export { createParticipant, findAll };
