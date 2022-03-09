cdc7a5b9fe3d87e5f571216dcd51fcabdde4180b
Fabian Losem <fabian.losem@web.de>
2022-03-09 16:15:49 +0100

    deno fmt

--- deps/web/deps.ts
+++ deps/web/deps.ts
@@ -1,5 +1,4 @@
 export { Bson, MongoClient } from "https://deno.land/x/mongo@v0.29.1/mod.ts";
 export { Application, Context, Router } from "https://deno.land/x/oak/mod.ts";
 export { config } from "https://deno.land/x/dotenv@v1.0.1/mod.ts";
-
 export { parse } from "https://deno.land/std@0.69.0/flags/mod.ts";
