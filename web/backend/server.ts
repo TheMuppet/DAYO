0735d8e0ebc647f6c0556239e27f38f051aecd79
Fabian Losem <fabian.losem@web.de>
2022-03-09 16:14:03 +0100

    add routes to server.ts

--- web/backend/server.ts
+++ web/backend/server.ts
@@ -1,10 +1,12 @@
 import { Application } from "../../deps/web/deps.ts";
 import { parse } from "../../deps/web/deps.ts";
+import router from "./routes/routes.ts";
 
 const { args } = Deno;
 const port: number = parse(args).port || 8080;
 const app = new Application();
 
+app.use(router.routes());
 app.use(async (context, next) => {
   try {
     await context.send({
