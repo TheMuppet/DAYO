db9f7911d59a6336123af51ea49a35ab096ffd94
Fabian Losem <fabian.losem@web.de>
2022-03-11 13:40:35 +0100

    add matchcontroller
    - function for adding matches

---
+++ web/backend/controller/matchController.ts
@@ -0,0 +1,25 @@
+import { Matches } from "../schemas/matches.ts";
+import { Context } from "../../../deps/web/deps.ts";
+
+// creates a new participant with data in request
+const createMatches = async (ctx: Context) => {
+  try {
+    const body = await ctx.request.body();
+    const { matches } = await body.value;
+    const data = await Matches.insertOne({
+      matches: matches,
+    });
+    ctx.response.body = { status: true, data: data };
+    ctx.response.status = 201;
+  } catch (_err) {
+    console.log(_err);
+    ctx.response.body = {
+      status: false,
+      error: _err.name,
+      error_message: _err.message,
+    };
+    ctx.response.status = 500;
+  }
+};
+
+export { createMatches };
