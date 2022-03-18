90f6ec463659ad942b761fb0316662d00a60e98b
Fabian Losem <fabian.losem@web.de>
2022-03-18 11:28:04 +0100

    added versions

--- deps/web/deps.ts
+++ deps/web/deps.ts
@@ -1,5 +1,9 @@
 export { Bson, MongoClient } from "https://deno.land/x/mongo@v0.29.1/mod.ts";
-export { Application, Context, Router } from "https://deno.land/x/oak/mod.ts";
+export {
+  Application,
+  Context,
+  Router,
+} from "https://deno.land/x/oak@v10.2.1/mod.ts";
 export { config } from "https://deno.land/x/dotenv@v1.0.1/mod.ts";
 export { parse } from "https://deno.land/std@0.69.0/flags/mod.ts";
-export { oakCors } from "https://deno.land/x/cors/mod.ts";
+export { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
