7fb5aca15574c65f49251732d457ac08bfcc9304
Fabian Losem <fabian.losem@web.de>
2022-03-18 11:27:50 +0100

    update functions

--- web/backend/controller/matchController.ts
+++ web/backend/controller/matchController.ts
@@ -1,17 +1,18 @@
 import { Matches } from "../schemas/matches.ts";
-import { Context } from "../../../deps/web/deps.ts";
+import { Bson, Context } from "../../../deps/web/deps.ts";
 
 const createMatches = async (ctx: Context) => {
   try {
     const body = await ctx.request.body();
-    const { matches } = await body.value;
+    const { matches, season, episode } = await body.value;
     const data = await Matches.insertOne({
       matches: matches,
+      season: season,
+      episode: episode,
     });
     ctx.response.body = { status: true, data: data };
     ctx.response.status = 201;
   } catch (_err) {
-    console.log(_err);
     ctx.response.body = {
       status: false,
       error: _err.name,
@@ -21,4 +22,42 @@ const createMatches = async (ctx: Context) => {
   }
 };
 
-export { createMatches };
+const getMatches = async (ctx: Context) => {
+  try {
+    const allParticipant = await Matches.find({}).toArray();
+    ctx.response.body = { status: true, data: allParticipant };
+    ctx.response.status = 200;
+  } catch (error) {
+    ctx.response.body = {
+      status: false,
+      error: error.name,
+      error_message: error.message,
+    };
+    ctx.response.status = 500;
+  }
+};
+
+// any used in here
+const getMatch = async (
+  { params, response }: { params: { id: string }; response: any },
+) => {
+  try {
+    const id = params.id;
+    let betterId = new Bson.ObjectId("6221dce5822815bf6973a0e1");
+    if (id != null) {
+      betterId = new Bson.ObjectId(id);
+    }
+    const participant = await Matches.findOne({ _id: betterId });
+    response.body = { status: true, data: participant };
+    response.status = 200;
+  } catch (error) {
+    response.body = {
+      status: false,
+      error: error.name,
+      error_message: error.message,
+    };
+    response.status = 500;
+  }
+};
+
+export { createMatches, getMatch, getMatches };
