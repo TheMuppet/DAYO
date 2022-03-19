02813365e57dd6342b6433dedd9de88dd997381c
Tom Sparrow <6250143+xTomsko@users.noreply.github.com>
2022-03-19 16:57:32 +0100

    adjusted backend to schema changes

--- web/backend/controller/matchNightController.ts
+++ web/backend/controller/matchNightController.ts
@@ -1,16 +1,16 @@
-import { Matchnight } from "../db/schemas/matchNight.ts";
+import {MatchNight, MatchNightSchema} from "../db/schemas/matchNight.ts";
 import { Bson, Context } from "../../../deps/web/deps.ts";
 
 // creates a new matching night with data in request
 const createMatchNight = async (ctx: Context) => {
   try {
     const body = await ctx.request.body();
-    const { lights, season, mn_number, couples } = await body.value;
-    const data = await Matchnight.insertOne({
+    const { lights, season, episode, couples }: MatchNightSchema = await body.value;
+    const data = await MatchNight.insertOne({
       couples: couples,
       lights: lights,
       season: season,
-      mn_number: mn_number,
+      episode: episode,
     });
     ctx.response.body = { status: true, data: data };
     ctx.response.status = 201;
@@ -28,7 +28,7 @@ const createMatchNight = async (ctx: Context) => {
 // gets all matching nights from db
 const getMatchNights = async (ctx: Context) => {
   try {
-    const allMatchNights = await Matchnight.find({}).toArray();
+    const allMatchNights = await MatchNight.find({}).toArray();
     ctx.response.body = { status: true, data: allMatchNights };
     ctx.response.status = 200;
   } catch (error) {
@@ -49,7 +49,7 @@ const getMatchNight = async (
   try {
     const id = params.id;
     const betterId = new Bson.ObjectId(id);
-    const matchnight = await Matchnight.findOne({ _id: betterId });
+    const matchnight = await MatchNight.findOne({ _id: betterId });
     response.body = { status: true, data: matchnight };
     response.status = 200;
   } catch (error) {
