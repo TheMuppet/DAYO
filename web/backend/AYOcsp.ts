b9b8e92d501956fd517a98cfbe567544e488f4fc
Fabian Losem <fabian.losem@web.de>
2022-03-19 22:09:51 +0100

    init class ayocsp

---
+++ web/backend/AYOcsp.ts
@@ -0,0 +1,30 @@
+import { MatchBoxSchema } from "./db/schemas/matchBox.ts";
+import { MatchNightSchema } from "./db/schemas/matchNight.ts"
+
+export class AYO {
+    men: Array<string>;
+    woman: Array<string>;
+    person11: string;
+    matchbox: MatchBoxSchema;
+    matchnight: MatchNightSchema;
+    mgender: Array<string>;
+    lgender: Array<string>;
+
+
+    constructor(men: Array<string>, woman: Array<string>, person11: string, matchbox: MatchBoxSchema, matchnight: MatchNightSchema) {
+        this.men = men
+        this.woman = woman
+        if (men.length >= woman.length) {
+            this.mgender = men
+            this.lgender = woman
+        } else {
+            this.mgender = woman
+            this.lgender = men
+        }
+        this.person11 = person11
+        this.matchbox = matchbox
+        this.matchnight = matchnight
+    }
+}
+
+
