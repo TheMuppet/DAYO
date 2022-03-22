dd9f3dfe4228823f44962b168d66e63e10f0f7e1
Fabian Losem <fabian.losem@web.de>
2022-03-22 21:57:03 +0100

    init solver

--- deps/csp/deps.ts
+++ deps/csp/deps.ts
@@ -1,1 +1,2 @@
-export {solve, StatProc, CSP} from 'https://deno.land/x/satisfyer@v1.1.0/mod.ts';
+export { solve,StatProc } from 'https://deno.land/x/satisfyer@v1.1.0/mod.ts';
+export type { CSP } from 'https://deno.land/x/satisfyer@v1.1.0/mod.ts';
