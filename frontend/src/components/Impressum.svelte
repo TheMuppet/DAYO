7e031166ae0091c10c24d3dbdc0e1dd2444e6b7f
alexsc23 <s191825@student.dhbw-mannheim.de>
2022-03-25 18:10:10 +0100

    Fmt impressum

--- frontend/src/components/Impressum.svelte
+++ frontend/src/components/Impressum.svelte
@@ -7,46 +7,57 @@
     <div class='impressum justify-content-center'>
         <h1>Impressum</h1>
         <p>Angaben gemäß § 5 TMG</p>
-        <p>Tom Sperling
+        <p>
+            Tom Sperling
             <br>
-        Drosselkamp6
+            Drosselkamp6
             <br>
-        25335 Elmshorn
-            <br>
-        </p>
-        <p> <strong>Vertreten durch: </strong>
-            <br>
-        Tom Sperling
+            25335 Elmshorn
             <br>
         </p>
-        <p><strong>Kontakt:</strong>
+        <p>
+            <strong>Vertreten durch: </strong>
             <br>
-        Telefon: 0151-64014253
-            <br>
-        E-Mail: <a href='mailto:tom.sparrow@outlook.de'>tom.sparrow@outlook.de</a>
+            Tom Sperling
             <br>
         </p>
-        <p><strong>Haftungsausschluss: </strong>
+        <p>
+            <strong>Kontakt:</strong>
+            <br>
+            Telefon: 0151-64014253
+            <br>
+            E-Mail: <a href='mailto:tom.sparrow@outlook.de'>tom.sparrow@outlook.de</a>
+            <br>
+        </p>
+        <p>
+            <strong>Haftungsausschluss: </strong>
             <br>
             <br>
             <strong>Haftung für Links</strong>
             <br>
             <br>
-        Unser Angebot enthält Links zu externen Webseiten Dritter, auf deren Inhalte wir keinen Einfluss haben. 
-        Deshalb können wir für diese fremden Inhalte auch keine Gewähr übernehmen. 
-        Für die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich. 
-        Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf mögliche Rechtsverstöße überprüft. 
-        Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar. 
-        Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. 
-        Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Links umgehend entfernen.
-        <br><br><strong>Datenschutz</strong><br><br>
-        Die Nutzung unserer Webseite ist in der Regel ohne Angabe personenbezogener Daten möglich. 
-        Soweit auf unseren Seiten personenbezogene Daten (beispielsweise Name, Anschrift oder eMail-Adressen) erhoben werden, erfolgt dies, soweit möglich, stets auf freiwilliger Basis. 
-        Diese Daten werden ohne Ihre ausdrückliche Zustimmung nicht an Dritte weitergegeben. <br>
-        Wir weisen darauf hin, dass die Datenübertragung im Internet (z.B. bei der Kommunikation per E-Mail) Sicherheitslücken aufweisen kann. 
-        Ein lückenloser Schutz der Daten vor dem Zugriff durch Dritte ist nicht möglich. <br>
-        Der Nutzung von im Rahmen der Impressumspflicht veröffentlichten Kontaktdaten durch Dritte zur Übersendung von nicht ausdrücklich angeforderter Werbung und Informationsmaterialien wird hiermit ausdrücklich widersprochen. 
-        Die Betreiber der Seiten behalten sich ausdrücklich rechtliche Schritte im Falle der unverlangten Zusendung von Werbeinformationen, etwa durch Spam-Mails, vor.<br>
+            Unser Angebot enthält Links zu externen Webseiten Dritter, auf deren Inhalte wir keinen Einfluss haben. 
+            Deshalb können wir für diese fremden Inhalte auch keine Gewähr übernehmen. 
+            Für die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich. 
+            Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf mögliche Rechtsverstöße überprüft. 
+            Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar. 
+            Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. 
+            Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Links umgehend entfernen.
+            <br>
+            <br>
+            <strong>Datenschutz</strong>
+            <br>
+            <br>
+            Die Nutzung unserer Webseite ist in der Regel ohne Angabe personenbezogener Daten möglich. 
+            Soweit auf unseren Seiten personenbezogene Daten (beispielsweise Name, Anschrift oder eMail-Adressen) erhoben werden, erfolgt dies, soweit möglich, stets auf freiwilliger Basis. 
+            Diese Daten werden ohne Ihre ausdrückliche Zustimmung nicht an Dritte weitergegeben.
+            <br>
+            Wir weisen darauf hin, dass die Datenübertragung im Internet (z.B. bei der Kommunikation per E-Mail) Sicherheitslücken aufweisen kann. 
+            Ein lückenloser Schutz der Daten vor dem Zugriff durch Dritte ist nicht möglich.
+            <br>
+            Der Nutzung von im Rahmen der Impressumspflicht veröffentlichten Kontaktdaten durch Dritte zur Übersendung von nicht ausdrücklich angeforderter Werbung und Informationsmaterialien wird hiermit ausdrücklich widersprochen. 
+            Die Betreiber der Seiten behalten sich ausdrücklich rechtliche Schritte im Falle der unverlangten Zusendung von Werbeinformationen, etwa durch Spam-Mails, vor.
+            <br>
         </p>
         <br>
         Website Impressum erstellt durch <a href="https://www.impressum-generator.de">impressum-generator.de</a> von der <a href="https://www.kanzlei-hasselbach.de/" rel="nofollow">Kanzlei Hasselbach</a>
@@ -61,9 +72,7 @@
         align-items: center;
     }
     .page-padding {
-        padding: 60px;
         padding-top: 120px;
-        position: relative;
-        z-index: -1;
+        padding-bottom: 20px;
     }
 </style>
