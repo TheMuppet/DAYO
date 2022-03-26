a011155a7a8ac98031cbb0234056dedbdb817a5c
alexsc23 <s191825@student.dhbw-mannheim.de>
2022-03-26 15:16:47 +0100

    Fmt

--- frontend/src/stores.js
+++ frontend/src/stores.js
@@ -1,24 +1,24 @@
-import { writable } from 'svelte/store';
+import { writable } from "svelte/store";
 
 const createWritableStore = (key, startValue) => {
-    const { subscribe, set } = writable(startValue);
-    
-    return {
-      subscribe,
-      set,
-      useLocalStorage: () => {
-        const json = localStorage.getItem(key);
-        if (json) {
-          set(JSON.parse(json));
-        }
-        
-        subscribe(current => {
-          localStorage.setItem(key, JSON.stringify(current));
-        });
+  const { subscribe, set } = writable(startValue);
+
+  return {
+    subscribe,
+    set,
+    useLocalStorage: () => {
+      const json = localStorage.getItem(key);
+      if (json) {
+        set(JSON.parse(json));
       }
-    };
-  }
 
-export const ParticipantStore = createWritableStore('ParticipantStore', []);
+      subscribe((current) => {
+        localStorage.setItem(key, JSON.stringify(current));
+      });
+    },
+  };
+};
 
-export const MatchStore = createWritableStore('MatchStore', []);
+export const ParticipantStore = createWritableStore("ParticipantStore", []);
+
+export const MatchStore = createWritableStore("MatchStore", []);
